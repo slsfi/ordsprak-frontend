c1b7484af6843d605db6928e70ff8456f7ce9dde
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2024-08-08 16:23:32 +0300

    refactor(home-page): enable footer and add styles for footer logo

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -151,7 +151,7 @@ export const config: Config = {
         siteTitleOnImageOnSmallScreens: false
       },
       showContentGrid: false,
-      showFooter: false,
+      showFooter: true,
       showSearchbar: false
     },
     index: {
